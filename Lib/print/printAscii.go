a2d316ece0e87b2624dacd9ee5c687c91390e270
the1Riddle <elvisotienomboya@gmail.com>
2026-01-28 22:56:02 +0300

    just updated riddle branch with changes made from main

--- Lib/print/printAscii.go
+++ Lib/print/printAscii.go
@@ -1,24 +1,42 @@
 package print
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func indexToPrint(r rune) int {
 	return int(r - 32)
 }
 
-func AsciiArt(data string, dataList []string) []string {
-	runs := []rune(data)
-	newList := []string{}
+func AsciiArt(data string, dataList [][][]rune) {
+	data = strings.ReplaceAll(data, "\\n", "\n")
+	lines := strings.Split(data, "\n")
+	height := len(dataList[0])
 
-	for x := 0; x < len(runs); x++ {
-		run := runs[x]
-		if run == '\n' {
-			newList = append(newList, "\n")
+	for _, line := range lines {
+		rows := make([][]rune, height)
+		if line == "" {
+			fmt.Println()
 			continue
-		} else {
-			newList = append(newList, dataList[indexToPrint(run)])
 		}
+
+		for _, ch := range line {
+			idx := indexToPrint(ch)
+			visual := dataList[idx]
+
+			for x := 0; x < height; x++ {
+				rows[x] = append(rows[x], visual[x]...)
+			}
+		}
+
+		for _, row := range rows {
+			fmt.Println(string(row))
+		}
+		/**
+		if endLine < len(lines)-2 {
+			fmt.Println()
+		}
+		**/
 	}
-	fmt.Println(newList)
-	return newList
 }
